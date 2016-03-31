4dea0103365f88d5600a764cf2c583eb1c7176c1
soyal <345723940@qq.com>
2016-03-31 12:31:50 +0800

    更改浏览器检测方式，直接检测是否被jquery2.2所支持

--- Thesis/src/js/browserwarning.js
+++ Thesis/src/js/browserwarning.js
@@ -2,5 +2,7 @@
  * Created by Soyal on 2016/3/31.
  */
 (function(){
-    alert("你目前的浏览器使用本系统存在严重问题，请更换为chrome,firefox或者最新IE");
+    if(typeof $ == "undefined"){
+        document.body.innerHTML = "<h1 class='text-center'>你现在的浏览器版本过低，请使用现代浏览器（如chrome,Firefox,IE11）再试</h1>";
+    }
 })();
