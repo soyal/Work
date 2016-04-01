c96d1a6c55118dbfbf8815560c2760f75a3e15af
soyal <345723940@qq.com>
2016-04-01 13:04:17 +0800

    重新设置密码验证

--- Thesis/src/js/passwordalert.js
+++ Thesis/src/js/passwordalert.js
@@ -2,10 +2,25 @@
  * Created by Soyal on 2016/4/1.
  */
 (function(){
-    $.getJSON("../../WEB-INF/static/json/checkpass.json",function(data){
-        if(data && data.ispasswordtrue===false){
-console.log(data);
-            $("#J-passwordalert").show();
-        }
+    $("#J-submit").click(function(){
+        console.log($("#username").val());
+        console.log($("#password").val());
+        $.ajax("../../WEB-INF/static/json/checkpass.json",{
+            type : "post",
+            data : {
+                username : $("#username").val(),
+                password : $("#password").val()
+            },
+            success : function(data){
+                console.log(data);
+                if(data.ispasswordtrue){
+                    $("#J-login").submit();
+                }else{
+                    $("#J-passwordalert").show();
+                }
+            },
+            dataType : "json"
+        });
     });
+
 })();
